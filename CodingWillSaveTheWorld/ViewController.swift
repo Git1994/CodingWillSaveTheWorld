fa0b25fcb95c16753a03b8f99d2e6ed50e5a5a44
Linda Novicenoka <lindanovicenoka@lindas-macbook-air.local>
2018-11-03 13:33:41 +0200

    Initial Commit

---
+++ CodingWillSaveTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingWillSaveTheWorld
+//
+//  Created by Linda Novicenoka on 11/3/18.
+//  Copyright © 2018 Linda Novicenoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CodingWillSaveTheWorldTests/CodingWillSaveTheWorldTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodingWillSaveTheWorldTests.swift
+//  CodingWillSaveTheWorldTests
+//
+//  Created by Linda Novicenoka on 11/3/18.
+//  Copyright © 2018 Linda Novicenoka. All rights reserved.
+//
+
+import XCTest
+@testable import CodingWillSaveTheWorld
+
+class CodingWillSaveTheWorldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
